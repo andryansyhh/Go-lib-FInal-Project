cb1ecf144ba233c7cc19840ef9bdaccb2ff3f0aa
andryansyhh <andryansyhh@gmail.com>
2021-06-18 17:39:04 +0700

    add footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Footer from "./components/moleculs/footer/footer";
 import Header from "./components/moleculs/header/header.jsx";
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Footer/>
     </div>
   );
 }
