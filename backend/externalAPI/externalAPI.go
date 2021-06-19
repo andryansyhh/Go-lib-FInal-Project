4ce152843e48c172b4610243d871bb4e6a1f7d3e
marwanjuna <mrwnolii@gmail.com>
2021-06-19 10:48:11 +0700

    add content route from external API

---
+++ backend/externalAPI/externalAPI.go
@@ -0,0 +1,29 @@
+package externalAPI
+
+import (
+	"encoding/json"
+	"golib/entity"
+	"io/ioutil"
+	"net/http"
+)
+
+func ExternalAPI() (entity.Data, error) {
+
+	url := "https://newsapi.org/v2/everything?language=en&pageSize=100&sortBy=publishedAt&sources=hacker-news&apiKey=e067289e827641599e90278cad01aefb"
+
+	req, _ := http.NewRequest("GET", url, nil)
+
+	res, _ := http.DefaultClient.Do(req)
+
+	defer res.Body.Close()
+	body, _ := ioutil.ReadAll(res.Body)
+
+	// fmt.Println(res)
+	// fmt.Println(string(body))
+
+	var datas entity.Data
+
+	json.Unmarshal(body, &datas)
+
+	return datas, nil
+}
