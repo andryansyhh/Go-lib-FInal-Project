04cce5f035379be2f366cbaeab928e20d2fc46d2
Marwan Olii <mrwnolii@gmail.com>
2021-06-19 16:28:36 +0700

    add content-page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,19 @@
 import "./App.css";
 import Register from "./components/moleculs/pages/register";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import ContentPage from "./components/moleculs/pages/ContentPage";
 
 function App() {
   return (
-    <div className="App">
-      <Register/>
+    <div className="App position-relative">
+      <Router>
+        <Switch>
+          <Route path="/register">
+            <Register />
+          </Route>
+          <Route path="/news" exact component={ContentPage} />
+        </Switch>
+      </Router>
     </div>
   );
 }
