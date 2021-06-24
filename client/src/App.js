c6d465d7e911ebd99ce3b93c8c3544b945013a0d
andryansyhh <andryansyhh@gmail.com>
2021-06-24 13:28:08 +0700

    update file

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,6 @@ import "./App.css";
 import Register from "./components/moleculs/pages/register";
 import Login from "./components/moleculs/pages/login";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Header from "./components/moleculs/header/header";
-import Footer from "./components/moleculs/footer/footer";
 import AdminRoutes from "./components/routes/admin/AdminRoutes";
 import ContentPage from "./components/moleculs/pages/ContentPage";
 import LandingPage from "./components/moleculs/pages/LandingPage";
