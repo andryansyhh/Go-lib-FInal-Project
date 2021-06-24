874922286acd2d391ace02d9b70f77dc1a78c457
sonex23 <sonaccx7@gmail.com>
2021-06-25 04:36:49 +0700

    perbaiki tampilan detail category => mengubah font => menambahkan fitur logout =>

--- client/src/components/moleculs/header/header.jsx
+++ client/src/components/moleculs/header/header.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { useHistory } from "react-router";
 import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { logoutUser } from "../../../redux/user/userAction";
 
 const Header = () => {
   const history = useHistory();
-
+  const dispatch = useDispatch();
+  const accessToken = localStorage.getItem("access_token");
   return (
     <div>
       <nav className="navbar navbar-expand-lg navbar-light bg-white">
@@ -57,28 +60,46 @@ const Header = () => {
                   News
                 </Link>
               </li>
-              <li>
-                <Link
-                  onClick={() => {
-                    history.push("/login");
-                  }}
-                  className="nav-link active"
-                  aria-current="page"
-                >
-                  Login
-                </Link>
-              </li>
-              <li>
-                <Link
-                  onClick={() => {
-                    history.push("/register");
-                  }}
-                  className="nav-link active"
-                  aria-current="page"
-                >
-                  Register
-                </Link>
-              </li>
+              {accessToken ? (
+                <li>
+                  <Link
+                    onClick={(e) => {
+                      e.preventDefault();
+                      dispatch(logoutUser());
+                      history.push("/");
+                    }}
+                    className="nav-link active"
+                    aria-current="page"
+                  >
+                    Logout
+                  </Link>
+                </li>
+              ) : (
+                <>
+                  <li>
+                    <Link
+                      onClick={() => {
+                        history.push("/login");
+                      }}
+                      className="nav-link active"
+                      aria-current="page"
+                    >
+                      Login
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      onClick={() => {
+                        history.push("/register");
+                      }}
+                      className="nav-link active"
+                      aria-current="page"
+                    >
+                      Register
+                    </Link>
+                  </li>
+                </>
+              )}
             </ul>
           </div>
         </div>
--- client/src/components/moleculs/pages/DetailCategoriPage.jsx
+++ client/src/components/moleculs/pages/DetailCategoriPage.jsx
@@ -1,49 +1,81 @@
-import React, {useEffect} from "react"
+import React, { useEffect } from "react";
 import { Card, Button } from "react-bootstrap";
 import { fetchBooks } from "../../../redux/admin/book/adminBookAction";
 import Header from "../header/header";
 import { useDispatch, useSelector } from "react-redux";
 
+const DetailCategoriPage = () => {
+  const dispatch = useDispatch();
 
-const DetailCategoriPage = ()=>{
-    const dispatch = useDispatch()
+  const { books, isLoading } = useSelector((state) => state.adminBook);
 
-    const { books, isLoading } = useSelector(state => state.adminBook)
+  useEffect(() => {
+    dispatch(fetchBooks());
+  }, []);
 
+  console.log(books);
 
-    useEffect(() => {
-        dispatch(fetchBooks())
-    },[])
+  return (
+    <>
+      <Header />
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <h2 className="text-center">List All Books</h2>
+          </div>
+        </div>
+        <div className="row justify-content-between">
+          <div className="col-3">
+            <button className="btn btn-primary text-light">
+              Back to Category
+            </button>
+          </div>
+          <div className="col-3">
+            <form action="">
+              <div className="form-row">
+                <div className="col-10">
+                  <input
+                    type="text"
+                    name="search"
+                    id="search"
+                    className="form-control"
+                    placeholder="Search Books.."
+                  />
+                </div>
+                <div className="col-2">
+                  <button className="btn btn-primary">
+                    <li className="fa fa-search text-white"></li>
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+        <div className="row">
+          {books.data &&
+            books.data.map((book) => {
+              return (
+                <div className="col-6 col-md-3 mt-4">
+                  <Card className="shadow-sm">
+                    <Card.Img
+                      variant="top"
+                      src="https://www.iconpacks.net/icons/2/free-pdf-download-icon-2617-thumb.png"
+                    />
+                    <Card.Body>
+                      <Card.Title>{book.title}</Card.Title>
+                      {/* <Card.Text>{book.url_file}</Card.Text> */}
+                      <Button variant="primary" className="text-light">
+                        Go somewhere
+                      </Button>
+                    </Card.Body>
+                  </Card>
+                </div>
+              );
+            })}
+        </div>
+      </div>
+    </>
+  );
+};
 
-    console.log(books)
-
-
-    return(
-        <>
-        <Header/>
-        {books.data && books.data.map((book) =>{
-            return(
-                <>
-                <div className="container ">
-            <Card style={{ width: '15rem', marginTop: "2rem" }}>
-                <Card.Img variant="top" src="https://www.iconpacks.net/icons/2/free-pdf-download-icon-2617-thumb.png"/>
-                <Card.Body>
-                <Card.Title>{book.title}</Card.Title>
-                <Card.Text>
-                    {book.url_file}
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
-                </Card.Body>
-            </Card>
-</div>
-                </>
-            )
-
-        })}
-        
-        </>
-    )
-    
-}
-
-export default DetailCategoriPage
+export default DetailCategoriPage;
