dee85d8f1f1cec9dc0bc446b123a2ad40f1172b3
andryansyhh <andryansyhh@gmail.com>
2021-06-24 21:30:08 +0700

    fix navbar, styling login & register

--- client/src/components/moleculs/pages/register.jsx
+++ client/src/components/moleculs/pages/register.jsx
@@ -5,6 +5,7 @@ import Footer from "../footer/footer";
 import Header from "../header/header";
 import GoogleButton from "react-google-button";
 import { registerUser } from "../../../redux/user/userAction";
+import imageRegis from "../../../assets/Mobile-login-pana.svg";
 
 const Register = () => {
   const userRegisterData = useSelector((state) => state.user);
@@ -35,59 +36,64 @@ const Register = () => {
       <div className="container-fluid page-container">
         <div className="container">
           <div className="row justify-content-center align-items-center">
-            <div className="col-sm"></div>
-            <div className="col-sm form-container"></div>
-            <Form className="" onSubmit={registerSubmit}>
-              <Form.Group className="" controlId="formBasicEmail">
-                <Form.Control
-                  type="fullname"
-                  placeholder="Enter FullName"
-                  onChange={(e) => {
-                    e.preventDefault();
-                    setName(e.target.value);
-                  }}
-                />
-              </Form.Group>
-              <Form.Group className="" controlId="formBasicEmail">
-                <Form.Control
-                  type="name"
-                  placeholder="Enter Username"
-                  onChange={(e) => {
-                    e.preventDefault();
-                    setUserName(e.target.value);
-                  }}
-                />
-              </Form.Group>
-              <Form.Group className="" controlId="formBasicEmail">
-                <Form.Control
-                  type="email"
-                  placeholder="Enter email"
-                  onChange={(e) => {
-                    e.preventDefault();
-                    setEmail(e.target.value);
-                  }}
-                />
-                <Form.Text className="text-muted"></Form.Text>
-              </Form.Group>
+            <div className="col-sm">
+              <div className="img-container">
+                <img src={imageRegis} alt="" />
+              </div>{" "}
+            </div>
+            <div className="col-sm form-container">
+              <Form className="" onSubmit={registerSubmit}>
+                <Form.Group className="" controlId="formBasicEmail">
+                  <Form.Control
+                    type="fullname"
+                    placeholder="Enter FullName"
+                    onChange={(e) => {
+                      e.preventDefault();
+                      setName(e.target.value);
+                    }}
+                  />
+                </Form.Group>
+                <Form.Group className="" controlId="formBasicEmail">
+                  <Form.Control
+                    type="name"
+                    placeholder="Enter Username"
+                    onChange={(e) => {
+                      e.preventDefault();
+                      setUserName(e.target.value);
+                    }}
+                  />
+                </Form.Group>
+                <Form.Group className="" controlId="formBasicEmail">
+                  <Form.Control
+                    type="email"
+                    placeholder="Enter email"
+                    onChange={(e) => {
+                      e.preventDefault();
+                      setEmail(e.target.value);
+                    }}
+                  />
+                  <Form.Text className="text-muted"></Form.Text>
+                </Form.Group>
 
-              <Form.Group className="" controlId="formBasicPassword">
-                <Form.Control
-                  type="password"
-                  placeholder="Password"
-                  onChange={(e) => {
-                    e.preventDefault();
-                    setPass(e.target.value);
-                  }}
-                />
-              </Form.Group>
-              <Form.Group className="" controlId="formButton">
-                <Form.Control
-                  className="btn btn-primary"
-                  type="submit"
-                  value="Register"
-                />
-              </Form.Group>
-            </Form>
+                <Form.Group className="" controlId="formBasicPassword">
+                  <Form.Control
+                    type="password"
+                    placeholder="Password"
+                    onChange={(e) => {
+                      e.preventDefault();
+                      setPass(e.target.value);
+                    }}
+                  />
+                </Form.Group>
+                <Form.Group className="" controlId="formButton">
+                  <Form.Control
+                    className="btn btn-primary"
+                    type="submit"
+                    value="Register"
+                  />
+                </Form.Group>
+              </Form>
+            </div>
           </div>
           {/* <GoogleButton /> */}
         </div>
