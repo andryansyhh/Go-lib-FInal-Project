76cb364b33336b6835bff518af0883e25e2b2262
andryansyhh <andryansyhh@gmail.com>
2021-06-25 15:22:17 +0700

    add home page

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import AdminRoutes from "./components/routes/admin/AdminRoutes";
 import ContentPage from "./components/moleculs/pages/ContentPage";
 import LandingPage from "./components/moleculs/pages/LandingPage";
 import DetailCategoriPage from "./components/moleculs/pages/DetailCategoriPage";
+import HomePage from "./components/moleculs/pages/HomePage";
 
 function App() {
   return (
@@ -21,6 +22,8 @@ function App() {
           <Route path="/detailPage">
             <DetailCategoriPage />
           </Route>
+          <Route path="/home" exact component={HomePage} />
+
           <Route path="/news" exact component={ContentPage} />
           <Route path="/" exact component={LandingPage} />
           <AdminRoutes />
