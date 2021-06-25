01fd94afab4e1561a23bff33308f1f592135f854
ammarteuku05 <ammarteuku09@gmail.com>
2021-06-25 15:28:40 +0700

    update preview tapi masih failed untuk ambil filenya

--- client/src/components/moleculs/pages/DetailBookPage.jsx
+++ client/src/components/moleculs/pages/DetailBookPage.jsx
@@ -1,23 +1,45 @@
-import React from "react"
-import Header from "../header/header"
-import FilePreviewer from 'react-file-previewer';
-import ggg from "../../../assets/krs.pdf"
+import React, { useState } from 'react';
+// import Loader from '../pages/pdfControl/Loader';
+import { Document, Page, pdfjs } from 'react-pdf';
+import ControlPanel from '../pages/pdfControl/ControlPanel';
+// pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+pdfjs.GlobalWorkerOptions.workerSrc = 'pdf.worker.min.js';
 
-const DetailBookPage = () =>{
- return(
-    <>
-     <Header />
-     <div className="container">
-        <h4></h4>
-        {/* <FilePreviewer 
-            file={{
-                url:`${ggg}`
-            }}
-        /> */}
-        
-     </div>
-    </>
- )
-}
+const DetailBookPage = () => {
+  const [scale, setScale] = useState(1.0);
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+//   const [isLoading, setIsLoading] = useState(true);
 
-export default DetailBookPage
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+    // setIsLoading(true);
+  }
+
+  return (
+    <div>
+      {/* <Loader isLoading={isLoading} /> */}
+      <section
+        id="pdf-section"
+        className="d-flex flex-column align-items-center w-100"
+      >
+        <ControlPanel
+          scale={scale}
+          setScale={setScale}
+          numPages={numPages}
+          pageNumber={pageNumber}
+          setPageNumber={setPageNumber}
+          file="./mining.pdf" 
+        />
+        <Document
+          file="./mining.pdf"
+          onLoadSuccess={onDocumentLoadSuccess}
+        >
+          <Page pageNumber={pageNumber} scale={scale} />
+        </Document>
+      </section>
+    </div>
+  );
+};
+
+export default DetailBookPage;
