1d2797199563cd0c2cccc70b2550c3cb824479e5
marwanjuna <mrwnolii@gmail.com>
2021-06-25 13:36:02 +0700

    fix accessToken typo, add header n footer on dashboard admin

--- client/src/components/moleculs/pages/admin/book/UpdateBook.jsx
+++ client/src/components/moleculs/pages/admin/book/UpdateBook.jsx
@@ -2,14 +2,18 @@ import React from "react";
 import Sidebar from "../../../../admin/Sidebar";
 import style from "../../../../admin/user/styles.css";
 import UpdateBook from "../../../../admin/book/FormUpdateBook";
+import Header from "../../../header/header";
+import Footer from "../../../footer/footer";
 
 const UpdateBookPage = () => {
   return (
     <>
+      <Header />
       <div className="d-flex mt-3" id="wrapper">
         <Sidebar />
         <UpdateBook />
       </div>
+      <Footer />
     </>
   );
 };
--- client/src/components/moleculs/pages/admin/user/AddUser.jsx
+++ client/src/components/moleculs/pages/admin/user/AddUser.jsx
@@ -2,14 +2,18 @@ import React from "react";
 import Sidebar from "../../../../admin/Sidebar";
 import style from "../../../../admin/user/styles.css";
 import CreateUser from "../../../../admin/user/FormAddUser";
+import Header from "../../../header/header";
+import Footer from "../../../footer/footer";
 
 const CreateUserPage = () => {
   return (
     <>
+      <Header />
       <div className="d-flex mt-3" id="wrapper">
         <Sidebar />
         <CreateUser />
       </div>
+      <Footer />
     </>
   );
 };
